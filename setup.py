8dcc577c1edc6ccf5d96bfebd13e9667cb10921a
mati <matizk@gmail.com>
2023-11-05 21:44:38 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='greeclimateapi',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/matizk144/greeclimateapi',
+    license='MIT',
+    author='matizk144',
+    author_email='matizk@gmail.com',
+    description='Gree API'
+)
